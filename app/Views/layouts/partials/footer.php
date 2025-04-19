4cafd3fcbb6d88ce535ba24da9b078c68eacb0ca
vinss-droid <kevinsipahutar220604@gmail.com>
2025-04-19 23:27:20 +0700

    change app name

--- app/Views/layouts/partials/footer.php
+++ app/Views/layouts/partials/footer.php
@@ -2,7 +2,7 @@
 
 ?>
 <footer class="main-footer">
-  <strong>Copyright &copy; 2024 <a href="#">Your Application</a>.</strong>
+  <strong>Copyright &copy; 2024 <a href="#">Apotek Arrozaq</a>.</strong>
   All rights reserved.
   <div class="float-right d-none d-sm-inline-block">
     <b>Version</b> 1.0.0
